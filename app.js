6b5bf74a497a06bebc3abaad1a5c623628a24aab
Klace Koch <koch.klace@gmail.com>
2020-07-23 19:26:51 -0700

    Second question done

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 'use strict'
 
-var userName = prompt('Hello, I\'m Klace, whats your name?')
-  alert('Hi '+userName+ ', answer the following questions with a yes or no response and learn more about me...sound good?')
-  console.log('User name: '+userName)
+var userName = prompt('Hello, I\'m Klace, whats your name?');{
+  alert('Hi '+userName+ ', answer the following questions with a "y" or "n" response and learn more about me...sound good?');
+  console.log('User name: '+userName);
+}
+var fromSea = prompt('Do you think I\'m originally from Seattle?(y/n)');
+  if (fromSea==='y'){
+    alert ('Incorrect. I was raised in Hamilton, Montana.')
+  } else { (fromSea === 'n')
+    alert ('Thats right, I was raised in Hamilton, Montana.')
+}
