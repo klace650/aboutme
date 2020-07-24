5541e9f05e70e81a5aed7a3d354636105f62d9b3
Klace Koch <koch.klace@gmail.com>
2020-07-23 19:42:59 -0700

    Fourth question done

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ var userName = prompt('Hello, I\'m Klace, whats your name?');{
   console.log('User name: '+userName);
 }
 var fromSea = prompt('Do you think I\'m originally from Seattle?(y/n)');
-  if (fromSea==='y'){
-    alert ('Incorrect. I was raised in Hamilton, Montana.')
+  if (fromSea ==='y'){
+    alert ('Incorrect. I was raised in Hamilton, Montana.');
   } else { (fromSea === 'n')
-    alert ('That\'s right, I was raised in Hamilton, Montana.')
+    alert ('That\'s right, I was raised in Hamilton, Montana.');
 }
-var whatBranch = prompt ('I left Hamilton a month after graduation highschool and joined the military - do you think I joined the Air Force?')
+var whatBranch = prompt ('I left Hamilton a month after graduation highschool and joined the military - do you think I joined the Air Force?');
   if (whatBranch === 'y'){
-    alert ('Thats right, I joined the Air Force right out of highschool.')
+    alert ('Thats right, I joined the Air Force right out of highschool.');
   } else { (whatBranch==='n')
-    alert ('Wrong, I did join the Air Force where I was a munitions troop.')
+    alert ('Wrong, I did join the Air Force where I was an AMMO troop.');
+}
+var currentJob = prompt ('Is it true that I currently work as a barista at a bakery?');
+  if (currentJob === 'y'){
+    alert ('No, sorry. I\'m a bicycle mechanic!');
+  } else { (currentJob==='n')
+    alert ('You\'re right, my partner is though! She\'s great.');
 }
