24a10ff50b993233ce62d2a4cc2bc2a756438e44
Klace Koch <koch.klace@gmail.com>
2020-07-23 19:09:43 -0700

    initial commit - first question

---
+++ app.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var userName = prompt('Hello, I\'m Klace, whats your name?')
+  alert('Hi '+userName+ ', answer the following questions with a yes or no response and learn more about me...sound good?')
+  console.log('User name: '+userName)
